a3228c788412d883476920cff47679bd3b30b03c
vipul chakravarthy <vipulchakravarthy.r@gmail.com>
2021-10-10 16:09:26 +0530

    completed react bootstrap nav bar

--- react-common-ui/src/components/Navbar/ReactBootstrapNavbar.tsx
+++ react-common-ui/src/components/Navbar/ReactBootstrapNavbar.tsx
@@ -7,12 +7,12 @@ import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
-const ReactBootstrapNavbar = () => {
+const ReactBootstrapNavbar:React.FC = () => {
     return (
         <div>
             <Navbar bg="secondary" variant="dark">
                 <Navbar.Brand href="#home">
-                    Website-name
+                    Name of your site
                 </Navbar.Brand>
                 <Nav className="me-auto">
                 <Nav.Link href="#features">Features</Nav.Link>
@@ -25,7 +25,7 @@ const ReactBootstrapNavbar = () => {
                     <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                 </NavDropdown>
                 </Nav>
-                <Form className="d-flex">
+                <Form className="d-flex px-3">
                     <FormControl
                     type="search"
                     placeholder="Search"
